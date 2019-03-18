6c14b7ce1d8648c4aa0ae5cb3c9afe08a91043ab
Byron4j <1552546443@qq.com>
2019-03-18 13:30:38 +0800

    :tada: init repository

---
+++ src/main/java/org/byron4j/fuckjavathreadconcurrency/FuckjavaThreadConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package org.byron4j.fuckjavathreadconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckjavaThreadConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuckjavaThreadConcurrencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/byron4j/fuckjavathreadconcurrency/FuckjavaThreadConcurrencyApplicationTests.java
@@ -0,0 +1,16 @@
+package org.byron4j.fuckjavathreadconcurrency;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FuckjavaThreadConcurrencyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
