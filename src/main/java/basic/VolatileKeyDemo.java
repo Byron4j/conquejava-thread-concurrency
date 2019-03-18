081ec0d484dc3f4a8dd35ac6a459e7d5c6e256d5
Byron4j <1552546443@qq.com>
2019-03-18 18:14:10 +0800

    👉 👉 Volatile 关键字Demo示例

---
+++ src/main/java/basic/VolatileKeyDemo.java
@@ -0,0 +1,41 @@
+package basic;
+
+import utils.Utils;
+
+/**
+ * volatile 关键字用法
+ * stop 如果不是 volatile 修饰，则thread启动后，看到的是自己线程栈的变量值false，陷入死循环；
+ * 如果被 volatile 修饰，则 stop 变量是线程可见的，stop 设置为true后，thread 会退出 while 循环
+ */
+public class VolatileKeyDemo {
+    volatile boolean stop = false;
+
+    public static void main(String[] args) {
+
+        VolatileKeyDemo demo = new VolatileKeyDemo();
+
+        Thread thread = new Thread(demo.getConcurrencyCheckTask());
+        thread.start();
+
+        Utils.sleep(1000);
+        System.out.println("Set stop to true in main!");
+        demo.stop = true;
+        System.out.println("Exit main.");
+    }
+
+    ConcurrencyCheckTask getConcurrencyCheckTask() {
+        return new ConcurrencyCheckTask();
+    }
+
+    private class ConcurrencyCheckTask implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("ConcurrencyCheckTask started!");
+            // 如果主线中stop的值可见，则循环会退出。
+            // 在我的开发机上，几乎必现循环不退出！（简单安全的解法：在 stop 属性上加上volatile）
+            while (!stop) {
+            }
+            System.out.println("ConcurrencyCheckTask stopped!");
+        }
+    }
+}
